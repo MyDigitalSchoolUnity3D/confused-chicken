f3b9f08d35c5d041362cb3d53c29552305e164e1
gregorie536 <vexier.gregorie@gmail.com>
2025-03-04 14:26:00 +0100

    Déplacement et collision

--- Assets/02-Code/ChickenController.cs
+++ Assets/02-Code/ChickenController.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ChickenController : MonoBehaviour
 {
-    public float speed = 5f;  // Vitesse de la poule
-    private Vector2 direction = Vector2.right; // Direction initiale
+    public float speed = 3f;  // Vitesse de la poule
+    private Vector2 direction = Vector2.right; // Direction
+    private TextMeshProUGUI gameOverText;
+
+    void Start()
+    {
+        // Trouve l'objet du texte dans la scène
+        gameOverText = GameObject.Find("GameOverText").GetComponent<TextMeshProUGUI>();
+        gameOverText.gameObject.SetActive(false); // Cache le texte au début
+    }
 
     void Update()
     {
         HandleInput();
+        CheckScreenWrap();
     }
 
     void FixedUpdate()
@@ -29,7 +41,6 @@ public class ChickenController : MonoBehaviour
 
     void ChangeDirection(Vector2 newDirection)
     {
-        // Empêche de faire demi-tour sur soi-même
         if (newDirection != -direction)
         {
             direction = newDirection;
@@ -39,5 +50,39 @@ public class ChickenController : MonoBehaviour
     void Move()
     {
         transform.position += (Vector3)direction * speed * Time.fixedDeltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Mort"))
+        {
+            Debug.Log("Ah AH AH gros naze t'es mort !");
+            StartCoroutine(GameOver());
+        }
+    }
+
+    IEnumerator GameOver()
+    {
+        gameOverText.gameObject.SetActive(true); // Affiche le message
+        yield return new WaitForSeconds(2f); // Attend 2 secondes
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Recharge la scène
+    }
+
+    void CheckScreenWrap()
+    {
+        Vector3 pos = transform.position;
+        float screenLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+        float screenRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+
+        if (pos.x < screenLeft)
+        {
+            pos.x = screenRight;
+        }
+        else if (pos.x > screenRight)
+        {
+            pos.x = screenLeft;
+        }
+
+        transform.position = pos;
     }
 }
